561af9c8776e0c2c24aa7a2b83062f49415792cb
azendal <trasvina@gmail.com>
2010-10-29 23:42:51 -0500

    tests now are using more accurate examples reflecting the language behaviour

--- spec/basic.js
+++ spec/basic.js
@@ -1,4 +1,4 @@
-Te.run(function(basicTests){
+Te.run(function(){
     
     // this.beforeEach(function(spec){
     //     spec.registry.x = {
@@ -11,13 +11,13 @@ Te.run(function(basicTests){
     this.spec('come up as pendant')();
     
     this.spec('do nothing but not break')(function(){
-        this.end();
+        this.completed();
     });
     
     this.spec('toBe assert must behave as expected')(function(){
         var x = [];
         this.expect(x).toBe(x);
-        this.end();
+        this.completed();
     });
     
     this.spec('toNotBe assert must behave as expected')(function(){
@@ -25,21 +25,21 @@ Te.run(function(basicTests){
         this.expect(x).toNotBe([]);
         this.expect([]).toNotBe([]);
         this.expect({}).toNotBe({});
-        this.end();
+        this.completed();
     });
     
     this.spec('toEqual assert must behave as expected')(function(){
         this.expect(0.5).toEqual(0.5);
-        this.expect([1].toString()).toEqual([1].toString());
-        this.expect({}).toEqual({});
-        this.end();
+        this.expect([1][0]).toEqual([1][0]);
+        this.expect(({attr : 5}).attr).toEqual(({attr : 5}).attr);
+        this.completed();
     });
     
     this.spec('toNotEqual assert must behave as expected')(function(){
         this.expect(0.5).toNotEqual(0.6);
-        this.expect([]).toNotEqual([1]);
-        this.expect({}).toNotEqual({ attr : 1});
-        this.end();
+        this.expect([]).toNotEqual([]);
+        this.expect({}).toNotEqual({});
+        this.completed();
     });
     
 });
